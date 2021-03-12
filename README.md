93fd3637f5abd6e89fcfd251383b2cb367a851c4
Toran Sharma <toransharma27@gmail.com>
2021-03-12 15:26:00 +0000

    Switch to using Quart-WebSocketRooms package instead of Quart directly

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Xporcle-Server
-Python web socket application built using the Quart framework to serve the Xporcle browser extension.
+Python web socket application built using the WebSocketRooms package for teh Quart framework to serve the Xporcle browser extension.
--- server.py
+++ server.py
@@ -1,333 +1,184 @@
-from quart import Quart, request, websocket
-import random, string, json, asyncio
+from quart_websocketrooms import WebSocketRooms, User, Room
+import json
+import asyncio
 
-app = Quart(__name__)
 
-rooms = dict()
+class User(User):
+    def __init__(self, *args, **kargs):
+        super().__init__()
+        self.url = None
+        self.score = {"score": 0, "wins": 0}
+        self.live_score = None #{"score": int, "finished": bool, "quiz_time": int}
+
+
+class Room(Room):
+    def __init__(self, *args, **kargs):
+        super().__init__(*args, **kargs)
+        self._live_scores = {}
+
+    @property
+    def urls(self):
+        return {username: self.users[username].url for username in self.users}
+
+    @property
+    def scores(self):
+        return {username: self.users[username].score for username in self.users}
+
+    @scores.setter
+    def scores(self, new_scores):
+        for username in new_scores:
+            if username in self.users:
+                self.users[username].score = new_scores[username]
+
+    @property
+    def live_scores(self):
+        self._live_scores.update({username: self.users[username].live_score for username in self.users})
+        return self._live_scores
+        
+    def reset_live_scores(self):
+        _live_scores = {}
+        for username in self.users:
+            self.users[username].live_score = {"score": 0, "finished": False, "quiz_time": 0}
+
+    @property
+    def quiz_running(self):
+        return None not in [self.live_scores[username] for username in self.users]
+
+    @property
+    def quiz_finished(self):
+        return False not in [self.live_scores[username]["finished"] for username in self.users]
+
+    async def update_scores(self):
+        score_list = [(username, quiz_data["score"], quiz_data["quiz_time"]) for username, quiz_data in self.live_scores.items()]
+        score_list.sort(key = lambda entry: entry[2]) # sort by quiz_time fist
+        score_list.sort(key = lambda entry: entry[1], reverse = True) # sort by score next
+
+        # Increment number of wins for the winner.
+        winner = score_list[0][0]
+        if winner in self.scores:
+            self.scores[winner]["wins"] += 1
+        
+        rankings = [entry[0] for entry in score_list]
+
+        points = calculatePoints(rankings)
+
+        for username, num_points in points.items():
+            if username in self.users:
+                self.scores[username]["score"] += num_points
+            else:
+                # The player left during the quiz, don't give them points
+                pass
+        
+        self.reset_live_scores()
+        await self.broadcast({"type": "scores_update", "scores": self.scores})
+
+    async def send_live_scores_update(self):
+        await self.broadcast({"type": "live_scores_update", "live_scores": self.live_scores})
+        if self.quiz_finished:
+            await self.broadcast({"type": "quiz_finished"})
+            await self.update_scores()
+
+
+app = WebSocketRooms(__name__, CustomRoomClass=Room, CustomUserClass=User)
 
 @app.route("/xporcle")
 async def hello_world():
-	return "This URL is used for websocket communications with the <a href='https://github.com/ToranSharma/Xporcle-Server'>Xporcle Server</a> for <a href='https://github.com/ToranSharma/Xporcle-Extension'>Xporcle, the sporcle multiplayer browser extension</a>\n"
+    return "This URL is used for websocket communications with the <a href='https://github.com/ToranSharma/Xporcle-Server'>Xporcle Server</a> for <a href='https://github.com/ToranSharma/Xporcle-Extension'>Xporcle, the sporcle multiplayer browser extension</a>\n"
 
-@app.websocket("/xporcle")
-async def ws():
-	code = None
-	username = None
-	url = None
-	queue = asyncio.Queue()
+@app.incoming_processing_step
+async def url_update(user, message):
+    step_responses = []
+    if message["type"] == "url_update":
+        url = message["url"]
+        user.url = url
+        await user.room.send_to_hosts({"type": "url_update", "username": user.username, "url": url})
+    return step_responses
 
-	async def processMessages(queue):
-		nonlocal code, username, url
-		while True:
-			raw_data = await websocket.receive()
-			message = json.loads(raw_data)
+@app.incoming_processing_step
+async def live_scores_update(user, message):
+    step_responses = []
+    if message["type"] == "live_scores_update":
+        if message["finished"]:
+            user.live_score["finished"] = True
+        else:
+            user.live_score["score"] = message["current_score"]
+            user.live_score["quiz_time"] = message["quiz_time"]
+        await user.room.send_live_scores_update()
+    return step_responses
 
-			response = None
+@app.incoming_processing_step
+async def start_countdown(user, message):
+    step_responses = []
+    if message["type"] == "start_coutdown":
+        user.room.broadcast({"type": "start_countdown"})
+    return step_responses
 
-			message_type = message["type"]
-			
-			if message_type == "create_room":
-				username = message["username"]
-				url = message["url"]
-				code = await createRoom(username, queue, url)
-				response = {"type": "scores_update", "scores": rooms[code]["scores"]}
+@app.incoming_processing_step
+async def start_quiz(user, message):
+    step_responses = []
+    if message["type"] == "start_quiz" and not user.room.quiz_running:
+        user.room.reset_live_scores()
+        await user.room.broadcast({"type": "start_quiz"})
+    return step_responses
 
-			elif message_type == "close_room" and code in rooms:
-				await closeRoom(code)
-				break
+@app.incoming_processing_step
+async def page_disconnect(user, message):
+    step_responses = []
+    if message["type"] == "page_disconnect":
+        user.room.live_scores[user.username]["finished"] = True
+        await user.room.send_live_scores_update()
+    return step_responses
 
-			elif message_type == "join_room":
-				username = message["username"]
-				code = message["code"]
-				url = message["url"]
-				success = await joinRoom(code, username, queue, url)
-				if not success:
-					username = None
-					code = None
-					url = None
+@app.incoming_processing_step
+async def change_quiz(user, message):
+    step_responses = []
+    if message["type"] == "change_quiz":
+        await user.room.broadcast({"type": "change_quiz", "url": message["url"]})
+    return step_responses
 
-			elif message_type == "load_room":
-				username = message["username"]
-				url = message["url"]
-				code = await loadRoom(username, queue, url, message["scores"])
-				response = {"type": "scores_update", "scores": rooms[code]["scores"]}
-
-			elif message_type == "leave_room":
-				await broadcastToRoom(code, {"type": "removed_from_room", "username": username})
-				await removeFromRoom(code, username)
-				break
-
-			elif message_type == "save_room":
-				response = {"type": "save_data", "data": rooms[code]["scores"]}
-
-			elif message_type == "host_promotion":
-				await makeHost(code, message["username"])
-
-			elif message_type == "change_host":
-				await changeHost(code, username, message["username"])
-
-			elif message_type == "url_update":
-				url = message["url"]
-				await updateUrl(code, username, url)
-
-			elif message_type == "start_countdown":
-				await broadcastToRoom(code, {"type": "start_countdown"})
-
-			elif message_type == "start_quiz":
-				await startQuiz(code)
-
-			elif message_type == "live_scores_update":
-				await updateLiveScores(code, username, message["current_score"], message["finished"], message["quiz_time"])
-
-			elif message_type == "page_disconnect":
-				await updateLiveScores(code, username, None, True, None)
-
-			elif message_type == "change_quiz":
-				await broadcastToRoom(code, {"type": "change_quiz", "url": message["url"]})
-
-			elif message_type == "suggest_quiz":
-				message["username"] = username
-				await sendToHosts(code, message)
-
-			else:
-				response = {"type": "error", "error": "unknown message type"}
-
-			if response is not None:
-				await queue.put(response)
-
-	async def sendMessages(queue):
-		nonlocal username
-		while True:
-			to_send = await queue.get()
-			await websocket.send(json.dumps(to_send))
-
-	sender = asyncio.create_task(sendMessages(queue))
-	reciever = asyncio.create_task(processMessages(queue))
-	try:
-		await asyncio.gather(sender, reciever)
-	except asyncio.CancelledError:
-		await broadcastToRoom(code, {"type": "removed_from_room", "username": username})
-		await removeFromRoom(code, username)
-
-def generateCode():
-	code = "".join(random.choices(string.ascii_letters + string.digits, k=8))
-	while code in rooms:
-		code = "".join(random.choices(string.ascii_letters + string.digits, k=8))
-
-	return code
-
-def allocateRoom():
-	code = generateCode()
-	rooms[code] = dict()
-
-	return code
-	
-async def createRoom(username, queue, url):
-	code = allocateRoom()
-
-	rooms[code] = {
-		"loaded": False,
-		"players": {username: {"message_queue": queue, "host": True, "url": url}},
-		"scores": {username: {"score": 0, "wins": 0}},
-		"live_scores": {}
-	}
-	
-	print("There " + ("are" if len(rooms) != 1 else "is") + " now {0} room".format(len(rooms)) + ("s" if len(rooms) != 1 else ""), flush=True)
-	await queue.put({"type": "new_room_code", "room_code": code})
-	return code
-
-async def joinRoom(code, username, queue, url):
-	success = True
-	fail_reason = ""
-	if code in rooms:
-		if username not in rooms[code]["players"]:
-			rooms[code]["players"].update({username: {"message_queue": queue, "host": False, "url": url}})
-
-			if not rooms[code]["loaded"] or username not in rooms[code]["scores"]:
-				rooms[code]["scores"].update({username: {"score": 0, "wins": 0}})
-		else:
-			success = False
-			fail_reason = "username taken"
-
-	else:
-		fail_reason = "invalid code"
-		success = False
-
-	message = {"type": "join_room", "success": success}
-	if not success:
-		message.update({"fail_reason": fail_reason})
-	else:
-		message["hosts"] = [username for username, data in rooms[code]["players"].items() if data["host"]]
-
-	await queue.put(message)
-
-	if success:
-		await broadcastToRoom(code, {"type": "scores_update", "scores": rooms[code]["scores"]})
-		await sendToHosts(code, {"type": "url_update", "username": username, "url": url})
-
-	return success;
-
-async def loadRoom(username, queue, url, scores):
-	code = allocateRoom()
-	rooms[code] = {
-		"loaded": True,
-		"players": {username: {"message_queue": queue, "host": True, "url": url}},
-		"scores": scores,
-		"live_scores": {}
-	}
-	
-	print("There " + ("are" if len(rooms) != 1 else "is") + " now {0} room".format(len(rooms)) + ("s" if len(rooms) != 1 else ""), flush=True)
-	await queue.put({"type": "new_room_code", "room_code": code})
-	return code
-
-async def closeRoom(code):
-	message = {"type": "room_closed", "room_code": code}
-	await broadcastToRoom(code, message)
-	del rooms[code]
-
-async def removeFromRoom(code, username):
-	host = False
-	if code in rooms:
-		if username in rooms[code]["players"]:
-			host = rooms[code]["players"][username]["host"]
-			del rooms[code]["scores"][username]
-			del rooms[code]["players"][username]
-			# Don't delete from the live scores, they will be removed when it is cleaned up at the end of the quiz
-			# But do set their finished state to true so the quiz can end.
-			if (username in rooms[code]["live_scores"]):
-				await updateLiveScores(code, username, None, True, None)
-
-		
-		if len(rooms[code]["players"]) == 0:
-			# No one left in the room, so let's delete it.
-			del rooms[code]
-			print("There " + ("are" if len(rooms) != 1 else "is") + " now {0} room".format(len(rooms)) + ("s" if len(rooms) != 1 else ""), flush=True)
-		else:
-			# Still some players connected to the room, so send a score update to show the player being removed.
-			await broadcastToRoom(code, {"type": "scores_update", "scores": rooms[code]["scores"]})
-			
-			if host:
-				# Removed player was a host
-				await hostsUpdate(code)
-
-				# If they were the only host, then pick a new one
-				hosts_remaining = True in [player["host"] for player in rooms[code]["players"].values()]
-				if not hosts_remaining:
-					# Choose a random player, should be good enough to just pick the first player in rooms[code]["players"].keys()
-					await makeHost(code, list(rooms[code]["players"].keys())[0])
-
-async def makeHost(code, username):
-	player = rooms[code]["players"][username]
-	player["host"] = True
-	urls = {username: data["url"] for username, data in rooms[code]["players"].items()}
-	await player["message_queue"].put({"type": "host_promotion", "urls": urls})
-	await hostsUpdate(code)
-
-async def removeHost(code, username):
-	player = rooms[code]["players"][username]
-	player["host"] = False
-	await hostsUpdate(code)
-
-async def changeHost(code, old, new):
-	await makeHost(code, new)
-	await removeHost(code, old)
-
-async def hostsUpdate(code):
-	hosts = [username for username, data in rooms[code]["players"].items() if data["host"]]
-	await broadcastToRoom(code, {"type": "hosts_update", "hosts": hosts})
-
-async def broadcastToRoom(code, message):
-	if code in rooms:
-		players = rooms[code]["players"]
-		for player in players.values():
-			await player["message_queue"].put(message)
-
-async def sendToHosts(code, message):
-	hosts = {key: value for key, value in rooms[code]["players"].items() if value["host"]}
-	for host in hosts.values():
-		await host["message_queue"].put(message)
-
-async def startQuiz(code):
-	if len(rooms[code]["live_scores"]) == 0:
-		for username, data in rooms[code]["players"].items():
-			rooms[code]["live_scores"][username] = {"score": 0, "finished": False, "quiz_time": 0}
-		
-		await broadcastToRoom(code, {"type": "start_quiz"})
-	else:
-		# Quiz already started don't do anything
-		pass
-
-async def updateLiveScores(code, username, current_score, finished, quiz_time):
-	if finished:
-		# Finished so don't update the time or score
-		rooms[code]["live_scores"][username]["finished"] = finished
-	else:
-		# Not finished so update the score and time
-		rooms[code]["live_scores"][username] = {"score": current_score, "finished": finished, "quiz_time": quiz_time}
-
-	await broadcastToRoom(code, {"type": "live_scores_update", "live_scores": rooms[code]["live_scores"]})
-	if finished:
-		finished_states = [username["finished"] for username in rooms[code]["live_scores"].values()]
-		still_playing = False in finished_states # Test if any of the finished_states are False
-		if not still_playing:
-			# No players left playing, so we can score the quiz
-			await broadcastToRoom(code, {"type": "quiz_finished"})
-			await updateScores(code)
-			# Reset all the live scores ready for the next time startQuiz is called
-			rooms[code]["live_scores"] = {}
-
-async def updateScores(code):
-	# Allocate points based on the live_scores
-
-	score_list = [(username, quiz_data["score"], quiz_data["quiz_time"]) for username, quiz_data in rooms[code]["live_scores"].items()]
-	score_list.sort(key = lambda entry: entry[2]) # sort by quiz_time fist
-	score_list.sort(key = lambda entry: entry[1], reverse = True) # sort by score next
-
-	# Inrement number of wins for the winner.
-	winner = score_list[0][0]
-	if winner in rooms[code]["scores"]:
-		rooms[code]["scores"][winner]["wins"] += 1
-	
-	rankings = [entry[0] for entry in score_list]
-
-	points = calculatePoints(rankings)
-
-	for username, num_points in points.items():
-		if username in rooms[code]["scores"]:
-			rooms[code]["scores"][username]["score"] += num_points
-		else:
-			# The player left during the quiz, don't give them points
-			pass
-	
-	await broadcastToRoom(code, {"type": "scores_update", "scores": rooms[code]["scores"]})
+@app.incoming_processing_step
+async def suggest_quiz(user, message):
+    step_responses = []
+    if message["type"] == "suggest_quiz":
+        suggestion_message = message.copy()
+        suggestion_message["username"] = user.username
+        user.room.send_to_hosts(suggestion_message)
+    return step_responses
 
 def calculatePoints(rankings):
-	'''
-	Taking points for Mario Kart 8 system:
-	https://mariokart.fandom.com/wiki/Driver%27s_Points#Mario_Kart_8_and_Mario_Kart_8_Deluxe
-	Points awared for first 12 places
-	1st: 15, 2nd: 12, 3rd: 10, 4th-12th: 9 ... 1, >13th: 0
-	If fewer than 12 players, points awarded from 1 upward.
-	'''
-	points = [15, 12, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-	num_players = len(rankings)
-	
-	if num_players > 12:
-		zeros = [0] * (num_players - 12)
-		points.extend(zeros)
-	else:
-		points = points[ 12 - num_players : ]
-	
-	return dict(zip(rankings, points))
-	
-async def updateUrl(code, username, url):
-	rooms[code]["players"][username]["url"] = url
-	await sendToHosts(code, {"type": "url_update", "username": username, "url": url})
+    '''
+    Taking points for Mario Kart 8 system:
+    https://mariokart.fandom.com/wiki/Driver%27s_Points#Mario_Kart_8_and_Mario_Kart_8_Deluxe
+    Points awared for first 12 places
+    1st: 15, 2nd: 12, 3rd: 10, 4th-12th: 9 ... 1, >13th: 0
+    If fewer than 12 players, points awarded from 1 upward.
+    '''
+    points = [15, 12, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    num_players = len(rankings)
+    
+    if num_players > 12:
+        zeros = [0] * (num_players - 12)
+        points.extend(zeros)
+    else:
+        points = points[ 12 - num_players:]
+    
+    return dict(zip(rankings, points))
 
 
+@app.outgoing_processing_step
+async def join_room_add_hosts(user, message):
+    if message["type"] == "join_room" and message["success"]:
+        message["hosts"] = list(user.room.hosts)
+
+@app.outgoing_processing_step
+async def users_update_add_scores(user, message):
+    if message["type"] == "users_update":
+        message["scores"] = user.room.scores
+        
+@app.outgoing_processing_step
+async def host_promotion_add_urls(user, message):
+    if message["type"] == "host_promotion":
+        message["urls"] = user.room.urls
 
 
-
-
-
-
+app.websocket_rooms_route("/xporcle")
